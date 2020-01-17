1a21301c9286eb347fd6c3ce8f9a9c0b1b6ce1bb
Eric Vester <ericevester@gmail.com>
2020-01-17 12:24:17 -0500

    Hello again world Project

---
+++ src/WordOut/HelloAgain.java
@@ -0,0 +1,13 @@
+package WordOut;
+
+public class HelloAgain {
+    public static String wordPrinter(String w){
+        String saying = w;
+        return saying;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(wordPrinter("Hello again, World"));
+    }
+
+}
